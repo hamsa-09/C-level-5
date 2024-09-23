43fef8403c0dfefeac02e265b8fbca99465ee751
hamsa-09 <hamsavardhinibaskar@gmail.com>
2024-09-23 19:30:06 +0530

    changed code 1

--- BinaryToDecimal.c
+++ BinaryToDecimal.c
@@ -2,23 +2,23 @@
 # include<string.h>
 # include<math.h>
 
-int tobinary(char* s)
-{
-    int len  = strlen(s);
-    int result = 0;
-    int k = 0;
-    for(int i = len-1;i>=0;i--)
-    {
-        result += (s[i]-'0')*pow(2,k);
-        k++;
-    }
-    return result;
-}
+// int tobinary(char* s)
+// {
+//     int len  = strlen(s);
+//     int result = 0;
+//     int k = 0;
+//     for(int i = len-1;i>=0;i--)
+//     {
+//         result += (s[i]-'0')*pow(2,k);
+//         k++;
+//     }
+//     return result;
+// }
 
 int main()
 {
-char s[10];
-scanf("%s",s);
-int result = tobinary(s);
-printf("%d",result);
+// char s[10];
+// scanf("%s",s);
+// int result = tobinary(s);
+printf("result");
 }
